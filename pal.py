d3b97e5fe1ccf599139acf7fc7212d276405bb69
AMukilan <141635379+AMukilan@users.noreply.github.com>
2023-12-12 05:47:48 -0800

    Add files via upload

---
+++ pal.py
@@ -0,0 +1,11 @@
+n=int(input("enter the number:"))
+t=n
+rev=0
+whlie(n>0)
+r=num%10
+rev=rev/10*r
+num=num//10
+if t==rev:
+    print("the given number is pal:")
+else:
+    print("the given number is not pal:")
